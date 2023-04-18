9ad6292b2008e67adb6fa3486cc965b26cd4cd17
methul98 <methu089@gmail.com>
2023-04-18 15:13:22 +0530

    Changes was made in Application.py

--- application.py
+++ application.py
@@ -3,45 +3,40 @@ import io
 import pickle
 from PIL import Image
 from flask import Flask, render_template, request,send_file,send_from_directory
+from flask_cors import cross_origin
 from jinja2 import Environment
 from index import *
 import os
 import math
 import subprocess
-
-
 application=Flask(__name__)
-app=application 
+app=application
 app.secret_key = 'ArthoMate'
-
 # Add the b64encode filter to the Jinja2 environment
 env = Environment()
 env.filters['b64encode'] = base64.b64encode
 #home page
-@app.route('/')
-
+@app.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
 def home():
     return render_template('index.html')
-
-@app.route('/predict')#Here I am uploading the image 
+@app.route('/predict')#Here I am uploading the image
+@cross_origin()
 def predict():
-
     return render_template('predict.html')
-
 @app.route('/about')
+@cross_origin()
 def about():
     return render_template('about.html')
-
 @app.route('/result',methods=['GET','POST'])
+@cross_origin()
 def upload():
     if request.method=='POST':
         img=Image.open(request.files['uploadImage'])
         img_byte_arr = io.BytesIO()
         img.save(img_byte_arr, format='png')
         img_byte_arr = img_byte_arr.getvalue()
-
         encoded_img = base64.b64encode(img_byte_arr).decode('utf-8')
-
         #From here onwards the coding for the rest of the works
         #1.Conversion from 2D to 3D shape
         image, depth_map = predict_depth(img)
@@ -50,19 +45,15 @@ def upload():
         #2.Upload the image classification model
         #2.1 Image classification model
         res1=model1(img)
-
         #2.2Second model
         values=model2_val(img)
         with open('model_firstml','rb') as f:
              model2=pickle.load(f)
         res2=model2.predict(values)[0]
-
-
         # #2.3 Third Model
         with open('model_secondml','rb') as f:
             model3=pickle.load(f)
         res3 = model3.predict(values)[0]
-
         # #combine all the resutls
         fin_result= 0.15*res1+0.15*res2+0.70*res3
         print(fin_result)
@@ -74,36 +65,28 @@ def upload():
             fin_result = math.floor(fin_result)
         print(fin_result)
         return render_template('result.html',img_data=encoded_img,result=fin_result)
-       
     return render_template('predict.html')
-
 @app.route('/final_result',methods=['POST'])
+@cross_origin()
 def final_result():
     if request.method=='POST':
         doctrorRes=request.form['DoctorResult']
         modelRes=request.form['ModelResult']
         print(doctrorRes,modelRes)
         return render_template('final_result.html',model=modelRes,doctor=doctrorRes)
-    
     return ''
-
 @app.route('/download')
+@cross_origin()
 def download():
     filename = 'mesh.obj'
     directory = app.root_path + '/static/obj_files/'
     path = directory + filename
     return send_file(path, as_attachment=True)
-
 @app.route('/preview', methods=['POST'])
+@cross_origin()
 def preview():
     filename = 'X_RAY.pcd'  # Replace with the actual filename of the .pcd file
     file_path = os.path.join(app.root_path, 'static/pcd_files', filename)
     pcd = o3d.io.read_point_cloud(file_path)
     o3d.visualization.draw_geometries([pcd])
-
     return ''
-
-
-
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5000)
